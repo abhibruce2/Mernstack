0a3fe9afbbc15d0730f4dc852dd6c1f051a03b68
Abhi <75887508+abhibruce2@users.noreply.github.com>
2023-11-24 18:18:32 +0530

    Update SignIn.js

--- my-app/src/Api/SignIn.js
+++ my-app/src/Api/SignIn.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
-
+import { API } from '.';
 
   export async function Post (formData) {
     try {
-        const response = await axios.post('http://localhost:5000/signin', formData);
-
+        const response = await API.post('/signin',{
+ }, formData);
+// if(response){
+  console.log("res----------------", response.data);
   return response
+// 
+// }
       } catch (error) {
         console.error('There was an error submitting the post:', error.message);
       }
